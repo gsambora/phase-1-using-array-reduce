a9a00acdc6020e19c08f1f450df44593de0b174f
Hana <grace.sambora@gmail.com>
2023-05-12 17:33:01 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce(function(countObj, currentBatch){
+    //console.log("Current batch has "+currentBatch+". The total count so far is "+countObj+".")
+    return currentBatch + countObj;
+})
+
+console.log(totalBatteries)
